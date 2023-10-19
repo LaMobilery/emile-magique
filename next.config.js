113e0e36361ad4198e3f6a34e3799c83b40233e3
cedricrenouleau <ced241@gmail.com>
2023-10-19 15:10:14 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  assetPrefix: isProd ? "https://lamobilery.github.io/emile-magique/" : undefined,
+  images: isProd
+      ? {
+        loader: "custom",
+        loaderFile: "./app/loader.ts",
+      }
+      : undefined,
+};
+
+module.exports = nextConfig;
