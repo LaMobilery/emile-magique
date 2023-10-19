c6764afab12182224c7f51a894ef984544fdaeb2
Romain-Brement <rbrement@keyneosoft.fr>
2023-10-19 17:06:54 +0200

    add typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,7 @@ export default function Home() {
         <meta name="description" content="lol" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya"></link>
       </Head>
 
       <main className={styles.main}>
