9deca2275296d917d2c6ab6e94d978f480f61a1a
cedricrenouleau <ced241@gmail.com>
2023-10-19 17:08:18 +0200

    lol je commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
