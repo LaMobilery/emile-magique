6523818d27fcba9ce1f787bfc91a25b635c7a533
cedricrenouleau <ced241@gmail.com>
2023-10-19 16:51:31 +0200

    background color

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,8 @@ import styles from "@/styles/Home.module.css";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import Link from "next/link";
 import dates from "../utils/dates.json";
 
-const inter = Inter({ subsets: ["latin"] });
 
 const dataJson = dates;
 
@@ -35,7 +33,6 @@ export default function Home() {
               date !== null && setStartDate(date);
             }}
           />
-
           <p>{text}</p>
         </div>
       </main>
